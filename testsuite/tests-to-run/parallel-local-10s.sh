210ca9614c34bd9da92283c3a15fff8d1eb6576f
Ole Tange <ole@tange.dk>
2015-10-17 01:57:30 +0200

    Fixed bug #46214: Using --pipepart doesn't spawn multiple jobs in version 20150922.

--- testsuite/tests-to-run/parallel-local-10s.sh
+++ testsuite/tests-to-run/parallel-local-10s.sh
@@ -4,6 +4,25 @@
 # Each should be taking 10-30s and be possible to run in parallel
 # I.e.: No race conditions, no logins
 cat <<'EOF' | sed -e 's/;$/; /;s/$SERVER1/'$SERVER1'/;s/$SERVER2/'$SERVER2'/' | stdout parallel -vj0 -k --joblog /tmp/jl-`basename $0` -L1
+echo '### bug #46214: Using --pipepart doesnt spawn multiple jobs in version 20150922'
+  seq 1000000 > /tmp/num1000000; 
+  stdout parallel --pipepart --progress -a /tmp/num1000000 --block 10k -j0 true |grep 1:local
+
+echo '**'
+
+testhalt() { 
+  echo '### testhalt --halt '$1; 
+  (yes 0 | head -n 10; seq 10) | stdout parallel -kj4 --halt $1 'sleep {= $_=$_*0.3+1 =}; exit {}'; echo $?; 
+  (seq 10; yes 0 | head -n 10) | stdout parallel -kj4 --halt $1 'sleep {= $_=$_*0.3+1 =}; exit {}'; echo $?; 
+}; 
+export -f testhalt; 
+  parallel -kj0 testhalt ::: now,fail=0 now,fail=1 now,fail=2 now,fail=30%  now,fail=70% 
+    soon,fail=0 soon,fail=1 soon,fail=2 soon,fail=30% soon,fail=70% 
+    now,success=0 now,success=1 now,success=2 now,success=30% now,success=70% 
+    soon,success=0 soon,success=1 soon,success=2 soon,success=30% now,success=70%
+
+echo '**'
+
 echo '### Test --halt-on-error 0'; 
   (echo "sleep 1;true"; echo "sleep 2;false";echo "sleep 3;true") | parallel -j10 --halt-on-error 0; 
   echo $?; 
@@ -67,23 +86,11 @@ echo '### Test last dying print --halt-on-error -2';
 
 echo '**'
 
-testhalt() { 
-  echo '### testhalt --halt '$1; 
-  (yes 0 | head -n 10; seq 10) | stdout parallel -kj4 --halt $1 'sleep {= $_=$_*0.3+1 =}; exit {}'; echo $?; 
-  (seq 10; yes 0 | head -n 10) | stdout parallel -kj4 --halt $1 'sleep {= $_=$_*0.3+1 =}; exit {}'; echo $?; 
-}; 
-export -f testhalt; 
-  parallel -kj0 testhalt ::: now,fail=0 now,fail=1 now,fail=2 now,fail=30%  now,fail=70% 
-    soon,fail=0 soon,fail=1 soon,fail=2 soon,fail=30% soon,fail=70% 
-    now,success=0 now,success=1 now,success=2 now,success=30% now,success=70% 
-    soon,success=0 soon,success=1 soon,success=2 soon,success=30% now,success=70%
-
-echo '**'
-
 echo '### test memfree'
   parallel --memfree 1k echo Free mem: ::: 1k
   stdout parallel --timeout 3 --argsep II parallel --memfree 1t echo Free mem: ::: II 1t
 
 echo '**'
+
 
 EOF
