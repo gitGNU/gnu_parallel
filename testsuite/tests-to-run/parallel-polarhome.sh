c871cdd0a37323014fb204a7329e99bc6bd711e5
Ole Tange <ole@tange.dk>
2015-12-19 02:46:32 +0100

    Fixed bug #46708: controlmaster uses tempdir in $HOME. Passes testsuite.

--- testsuite/tests-to-run/parallel-polarhome.sh
+++ testsuite/tests-to-run/parallel-polarhome.sh
@@ -6,15 +6,21 @@ P_ALL="vax freebsd solaris openbsd netbsd debian alpha aix redhat hpux ultrix mi
 P_NOTWORKING="vax alpha openstep"
 P_NOTWORKING_YET="ultrix irix"
 
-P_WORKING="minix freebsd solaris openbsd netbsd debian aix redhat hpux qnx tru64 openindiana suse solaris-x86 mandriva ubuntu scosysv unixware dragonfly centos miros hurd raspbian macosx hpux-ia64 syllable pidora"
+P_WORKING="freebsd solaris openbsd netbsd debian aix redhat hpux qnx openindiana suse solaris-x86 mandriva ubuntu scosysv unixware dragonfly centos miros raspbian macosx hpux-ia64 pidora"
+P_TEMPORARILY_BROKEN="minix syllable tru64 hurd"
 
 P="$P_WORKING"
 POLAR=`parallel -k echo {}.polarhome.com ::: $P`
 S_POLAR=`parallel -k echo -S 1/{}.polarhome.com ::: $P`
-# Avoid the stupid /etc/issue.net banner at Polarhome: -oLogLevel=quiet
+
+# 20150414 --timeout 80 -> 40
+# 20151219 --retries 5 -> 2
+TIMEOUT=10
+RETRIES=2
 
 echo '### Tests on polarhome machines'
 echo 'Setup on polarhome machines'
+# Avoid the stupid /etc/issue.net banner at Polarhome: -oLogLevel=quiet
 stdout parallel -kj0 ssh -oLogLevel=quiet {} mkdir -p bin ::: $POLAR &
 
 test_empty_cmd() {
@@ -23,7 +29,7 @@ test_empty_cmd() {
     bin/perl bin/parallel echo ::: OK_with_empty_cmd
 }
 export -f test_empty_cmd
-stdout parallel -j0 -k --retries 5 --timeout 80 --delay 0.1 --tag \
+stdout parallel -j0 -k --retries $RETRIES --timeout $TIMEOUT --delay 0.1 --tag \
   --nonall --env test_empty_cmd -S macosx.polarhome.com test_empty_cmd > /tmp/test_empty_cmd &
 
 copy_and_test() {
@@ -36,8 +42,7 @@ copy_and_test() {
       perl -pe 's:/[a-z0-9_]+.arg:/XXXXXXXX.arg:gi; s/\d\d\d\d/0000/gi;'
 }
 export -f copy_and_test
-# 20150414 --timeout 80 -> 40
-stdout parallel -j0 -k --retries 5 --timeout 40 --delay 0.1 --tag  -v copy_and_test {} ::: $POLAR
+stdout parallel -j0 -k --retries $RETRIES --timeout $TIMEOUT --delay 0.1 --tag  -v copy_and_test {} ::: $POLAR
 
 # Test remote wrapper working on all platforms
 parallel -j0 --nonall -k $S_POLAR hostname
