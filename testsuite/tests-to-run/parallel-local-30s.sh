402d3960d4ea1ab16b00fbff37f8c9dfea3e3282
Ole Tange <ole@tange.dk>
2016-07-10 16:02:26 +0200

    parallel: Fixed bug #48449: memory leak.

--- testsuite/tests-to-run/parallel-local-30s.sh
+++ testsuite/tests-to-run/parallel-local-30s.sh
@@ -4,27 +4,51 @@
 # Each should be taking 30-100s and be possible to run in parallel
 # I.e.: No race conditions, no logins
 
-# Assume /tmp/shm is easy to fill up
-export SHM=/tmp/shm/parallel
-mkdir -p $SHM
-sudo umount -l $SHM
-sudo mount -t tmpfs -o size=10% none $SHM
+par_race_condition1() {
+    echo '### Test race condition on 8 CPU (my laptop)'
+    seq 1 5000000 > /tmp/parallel_race_cond
+    seq 1 10 | parallel -k "cat /tmp/parallel_race_cond | parallel --pipe --recend '' -k gzip >/dev/null; echo {}"
+    rm /tmp/parallel_race_cond
+}
 
-cat <<'EOF' | sed -e 's/;$/; /;s/$SERVER1/'$SERVER1'/;s/$SERVER2/'$SERVER2'/' | stdout parallel -vj4 -k --joblog /tmp/jl-`basename $0` -L1
-echo '### Test race condition on 8 CPU (my laptop)'; 
-  seq 1 5000000 > /tmp/parallel_test; 
-  seq 1 10 | parallel -k "cat /tmp/parallel_test | parallel --pipe --recend '' -k gzip >/dev/null; echo {}"; 
-  rm /tmp/parallel_test
+par_tmp_full() {
+    # Assume /tmp/shm is easy to fill up
+    export SHM=/tmp/shm/parallel
+    mkdir -p $SHM
+    sudo umount -l $SHM
+    sudo mount -t tmpfs -o size=10% none $SHM
 
-echo '**'
+    echo "### Test --tmpdir running full. bug #40733 was caused by this"
+    stdout parallel -j1 --tmpdir $SHM cat /dev/zero ::: dummy
+}
 
-echo "### Test --tmpdir running full. bug #40733 was caused by this"
-  stdout parallel -j1 --tmpdir $SHM cat /dev/zero ::: dummy
+par_bug_48290() {
+    echo "### bug #48290: round-robin does not distribute data based on business"
+    echo "Jobslot 1 is 256 times slower than jobslot 4 and should get much less data"
+    yes "$(seq 1000|xargs)" | head -c 30M |
+    parallel --tagstring {%} --linebuffer --compress -j4 --roundrobin --pipe --block 10k \
+      pv -qL '{= $_=int( $job->slot()**4/2+1) =}'0000 |
+      perl -ne '/^\d+/ and $s{$&}++; END { print map { "$_\n" } sort { $s{$b} <=> $s{$a} } keys %s}'
+}
 
-echo '**'
+par_memory_leak() {
+    a_run() {
+	seq $1 |time -v parallel true 2>&1 |
+	grep 'Maximum resident' |
+	field 6;
+    }
+    export -f a_run
+    echo "### Test for memory leaks"
+    echo "Of 10 runs of 1 job at least one should be bigger than a 3000 job run"
+    small_max=$(seq 10 | parallel a_run 1 | jq -s max)
+    big=$(a_run 3000)
+    if [ $small_max -lt $big ] ; then
+	echo "Bad: Memleak likely."
+    else
+	echo "Good: No memleak detected."	
+    fi
+}
 
-echo "### bug #48290: round-robin does not distribute data based on business"
-  echo "Jobslot 1 is 8 times slower than jobslot 8 and should get much less data"
-  seq 10000000 | parallel --tagstring {%} --linebuffer --compress -j8 --roundrobin --pipe --block 300k 'pv -qL {%}00000'| perl -ne '/^\d+/ and $s{$&}++; END { print map { "$_\n" } sort { $s{$a} <=> $s{$b} } keys %s}'
 
-EOF
+export -f $(compgen -A function | grep par_)
+compgen -A function | grep par_ | sort | parallel -j6 --tag -k '{} 2>&1'
